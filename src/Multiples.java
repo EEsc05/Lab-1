4ef93fb7a291c5011afb96d951478d9bf09a510d
emily <emily.escalante@mail.utoronto.ca>
2025-05-08 18:44:14 -0600

    fixed multiples it works now

--- src/Multiples.java
+++ src/Multiples.java
@@ -2,10 +2,13 @@ public class Multiples {
     public static void main(String[] args) {
         int tot = 0;
         for (int i = 0; i < 1000; i++) {
-           boolean m3lessthan1000 = i * 3 < 1000;
-           boolean m5lessthan1000 = i * 5 < 1000;
+           boolean m3lessthan1000 = (i * 3) < 1000;
+           boolean m5lessthan1000 = (i * 5) < 1000;
 
-           if (m3lessthan1000 && m5lessthan1000) {
+           if (m3lessthan1000) {
+               tot++;
+           }
+           if (m5lessthan1000) {
                tot++;
            }
         }
